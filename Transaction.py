69b5ab43b98a7ae2f458ce4b87af29cef6371d4c
Sultan <arthur.sultan@octo.com>
2020-02-28 17:30:03 +0100

    Code resultant en fin de seance

--- Transaction.py
+++ Transaction.py
@@ -1,10 +1,14 @@
 class Transaction:
 
-    def __init__(self):
+    def __init__(self, compte, type, date, montant, balance=None):
         # self._idTransaction = None
-        # self._compte = None
-        # self._type = None
-        # self._date = None
-        # self._balance = None
-        # self._montant = None
+        self._compte = compte
+        self._type = type
+        self._date = date
+        self._balance = balance
+        self._montant = montant
+        pass
+
+    @staticmethod
+    def sauvegarder_en_Bdd(transaction):
         pass
--- testTransactions.py
+++ testTransactions.py
@@ -1,8 +1,20 @@
 import unittest
 
+from Compte import Compte
+from Transaction import Transaction
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+
+    def test_sauvegarde_instance_de_transaction_retour_ok(self):
+        # Given
+        transaction = Transaction(Compte(1,1000), 'depot',
+        '25/02/2020', 500, 1500)
+
+        # When
+
+
+        # Then
         self.assertEqual(True, False)
 
 
